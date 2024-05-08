2a182f3a29ab087c1d87be69516c0c0774cb5d39
abhisirka2001 <abhitiwari.sirka2001@gmail.com>
2024-05-09 02:27:09 +0530

    final_push

--- inference.py
+++ inference.py
@@ -59,6 +59,10 @@ parser.add_argument('--rotate', default=False, action='store_true',
 parser.add_argument('--nosmooth', default=False, action='store_true',
                     help='Prevent smoothing face detections over a short temporal window')
 
+parser.add_argument('--face_default',default="../images (1).jfif", type =str,
+                    help='Prevent failing when no face detected')
+
+
 
 def get_smoothened_boxes(boxes, T):
     for i in range(len(boxes)):
@@ -72,27 +76,36 @@ def get_smoothened_boxes(boxes, T):
 def face_detect(images):
     results = []
     pady1, pady2, padx1, padx2 = args.pads
-
+    global indices 
+    indices = []
     s = time()
+    idx= 0
+    
+    for image, (rect,score) in zip(images, face_rect(images)):
+        if len(rect)==0 :
+            
 
-    for image, rect in zip(images, face_rect(images)):
-        if rect is None:
-            cv2.imwrite('temp/faulty_frame.jpg', image) # check this frame where the face was not detected.
-            raise ValueError('Face not detected! Ensure the video contains a face in all the frames.')
+            results.append([image, [0,0,0,0]])
+         
+            indices.append(idx)
+            idx=idx+1
+            
+            #cv2.imwrite('temp/faulty_frame.jpg', image) # check this frame where the face was not detected.
+            #raise ValueError('Face not detected! Ensure the video contains a face in all the frames.')
+        else:
+            
+            y1 = max(0, rect[1] - pady1)
+            y2 = min(image.shape[0], rect[3] + pady2)
+            x1 = max(0, rect[0] - padx1)
+            x2 = min(image.shape[1], rect[2] + padx2)
+            idx=idx+1
+            results.append([image[y1:y2, x1:x2], (y1, y2, x1, x2)])
+    
 
-        y1 = max(0, rect[1] - pady1)
-        y2 = min(image.shape[0], rect[3] + pady2)
-        x1 = max(0, rect[0] - padx1)
-        x2 = min(image.shape[1], rect[2] + padx2)
-
-        results.append([x1, y1, x2, y2])
 
     print('face detect time:', time() - s)
 
-    boxes = np.array(results)
-    if not args.nosmooth: boxes = get_smoothened_boxes(boxes, T=5)
-    results = [[image[y1: y2, x1:x2], (y1, y2, x1, x2)] for image, (x1, y1, x2, y2) in zip(images, boxes)]
-
+    
     return results
 
 
@@ -110,16 +123,27 @@ def datagen(frames, mels):
         face_det_results = [[f[y1: y2, x1:x2], (y1, y2, x1, x2)] for f in frames]
 
     for i, m in enumerate(mels):
-        idx = 0 if args.static else i%len(frames)
+        idx = 0 if args.static else i % len(frames)
         frame_to_save = frames[idx].copy()
         face, coords = face_det_results[idx].copy()
+        # Check the value and dimensions of face
+
+        # Check if the face region is empty
+        if len(coords) and face.size  == 0:
+            face = np.random.rand(args.img_size, args.img_size, 3) * 255
+            coords_batch.append([])
+            # Handle the case where the face region is empty
+            print("Skipping resizing as face region is empty")
+        else: 
+            face = cv2.resize(face, (args.img_size, args.img_size))
+            coords_batch.append(coords)
 
         face = cv2.resize(face, (args.img_size, args.img_size))
 
         img_batch.append(face)
         mel_batch.append(m)
         frame_batch.append(frame_to_save)
-        coords_batch.append(coords)
+  
 
         if len(img_batch) >= args.wav2lip_batch_size:
             img_batch, mel_batch = np.asarray(img_batch), np.asarray(mel_batch)
@@ -133,6 +157,7 @@ def datagen(frames, mels):
             yield img_batch, mel_batch, frame_batch, coords_batch
             img_batch, mel_batch, frame_batch, coords_batch = [], [], [], []
 
+
     if len(img_batch) > 0:
         img_batch, mel_batch = np.asarray(img_batch), np.asarray(mel_batch)
 
@@ -145,15 +170,17 @@ def datagen(frames, mels):
         yield img_batch, mel_batch, frame_batch, coords_batch
 
 mel_step_size = 16
+
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
 print('Using {} for inference.'.format(device))
 
 def _load(checkpoint_path):
     if device == 'cuda':
         checkpoint = torch.load(checkpoint_path)
     else:
-        checkpoint = torch.load(checkpoint_path,
-                                map_location=lambda storage, loc: storage)
+        checkpoint = torch.load(checkpoint_path,map_location=lambda storage, loc: storage)
+        
     return checkpoint
 
 def load_model(path):
@@ -170,6 +197,10 @@ def load_model(path):
     return model.eval()
 
 def main():
+    # Create directories if they don't exist
+    os.makedirs("frames", exist_ok=True)
+    os.makedirs("frames_with_indices", exist_ok=True)
+    os.makedirs("final_frames", exist_ok=True)
     args.img_size = 96
 
     if os.path.isfile(args.face) and args.face.split('.')[1] in ['jpg', 'png', 'jpeg']:
@@ -194,7 +225,7 @@ def main():
             if not still_reading:
                 video_stream.release()
                 break
-
+            
             aspect_ratio = frame.shape[1] / frame.shape[0]
             frame = cv2.resize(frame, (int(args.out_height * aspect_ratio), args.out_height))
             # if args.resize_factor > 1:
@@ -266,13 +297,15 @@ def main():
 
         pred = pred.cpu().numpy().transpose(0, 2, 3, 1) * 255.
 
-        for p, f, c in zip(pred, frames, coords):
-            y1, y2, x1, x2 = c
-            p = cv2.resize(p.astype(np.uint8), (x2 - x1, y2 - y1))
-
-            f[y1:y2, x1:x2] = p
-            out.write(f)
-
+        for p, f, c, idx in zip(pred, frames, coords, range(len(frames))):
+            if len(c) == 0:  # Check if there are no face coordinates (no face detected)
+                out.write(f)  # Write the original frame without lip-syncing
+            else:
+                y1, y2, x1, x2 = c
+                p = cv2.resize(p.astype(np.uint8), (x2 - x1, y2 - y1))
+                f[y1:y2, x1:x2] = p
+                out.write(f)  # Write the lip-synced frame to the output videon the 'frames' directory
+        
     out.release()
 
     print("wav2lip prediction time:", time() - s)
@@ -306,16 +339,30 @@ face_batch_size = 64 * 8
 
 def face_rect(images):
     num_batches = math.ceil(len(images) / face_batch_size)
-    prev_ret = None
     for i in range(num_batches):
         batch = images[i * face_batch_size: (i + 1) * face_batch_size]
         all_faces = detector(batch)  # return faces list of all images
         for faces in all_faces:
             if faces:
-                box, landmarks, score = faces[0]
-                prev_ret = tuple(map(int, box))
-            yield prev_ret
-
+                for face in faces:
+                    box, landmarks, score = face
+                    yield tuple(map(int, box)), score
+            else:
+                # If no faces are detected, yield a placeholder value
+                yield (0, 0, 0, 0), 0
+                
+def face_rect_single(image):
+    faces = detector([image])  # return faces list of the image
+    if faces:
+        try:
+            box, landmarks, score = faces[0]
+            return tuple(map(int, box))
+        except (ValueError, TypeError):
+            # If unpacking fails, return the entire image as the face region
+            return (0, 0, image.shape[1], image.shape[0])
+    else:
+        # If no faces are detected, return None
+        return None
 
 if __name__ == '__main__':
     args = parser.parse_args()
